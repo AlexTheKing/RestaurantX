ca8fb6f6c55c87571ac1ce40d1687a80a98e5e62
Alex Beytuk <alexbeytuk@gmail.com>
2016-12-27 16:05:14 +0300

    little fix

--- backend/src/main/java/com/example/alex/restaurantx/backend/BackendController.java
+++ backend/src/main/java/com/example/alex/restaurantx/backend/BackendController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class BackendController {
+public class BackendController {
 
     @RequestMapping("/api/types")
     public String getTypes() {
--- backend/src/main/java/com/example/alex/restaurantx/backend/RestaurantXBackendApplication.java
+++ backend/src/main/java/com/example/alex/restaurantx/backend/RestaurantXBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class RestaurantXBackendApplication {
+public class RestaurantXBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantXBackendApplication.class, args);
